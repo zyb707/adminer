cb9ac28c656400c3f10f4141fead2a97703f24e9
zhuyunbao <zhuyunbao@mobifun365.net>
2016-10-27 12:22:12 +0800

    添加说明

--- adminer/include/config.inc.php
+++ adminer/include/config.inc.php
@@ -1,4 +1,21 @@
 <?php
+/**
+	vendor    : 类型
+		server: mysql 
+		sqlite: SQLite 3 
+		sqlite2: SQLite 2 
+		pgsql: PostgreSQL
+		oracle: Oracle
+		mssql: MS SQL
+		firebird: Firebird (alpha)
+		simpledb: SimpleDB
+		mongo: MongoDB (beta)
+		elastic: Elasticsearch (beta)
+	server   ：host 地址
+	username ：用户名
+	password ：密 码
+	db 		 ：数据库 可不填
+*/
 $servers = array(
 	array(
 		'vendor'=>'server',
